08929f96fa710b5ea19ddb07acbdf0b06e820874
jox-gh <mail@jaydonoxley.com>
2025-10-22 11:25:44 +1000

    fixed again

--- src/etl_tutorial/defs/assets.py
+++ src/etl_tutorial/defs/assets.py
@@ -4,17 +4,7 @@ import duckdb
 import filelock
 
 @dg.asset
-def assets(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
-
-def serialize_duckdb_query(duckdb_path: str, sql: str):
-    """Execute SQL statement with file lock to guarantee cross-process concurrency."""
-    lock_path = f"{duckdb_path}.lock"
-    with filelock.FileLock(lock_path):
-        conn = duckdb.connect(duckdb_path)
-        try:
-            return conn.execute(sql)
-        finally:
-            conn.close()
+def assets(context: dg.AssetExecutionContext) -> dg.MaterializeResult:
 
 def import_url_to_duckdb(url: str, duckdb: DuckDBResource, table_name: str):
     with duckdb.get_connection() as conn:
@@ -28,37 +18,29 @@ def import_url_to_duckdb(url: str, duckdb: DuckDBResource, table_name: str):
         assert row_count is not None
         row_count = row_count[0]
 
-@dg.asset(
-    kinds={"duckdb"},
-    key=["target", "main", "raw_customers"],
-)
+@dg.asset(kinds={"duckdb"}, key=["target", "main", "raw_customers"])
 def raw_customers(duckdb: DuckDBResource) -> None:
     import_url_to_duckdb(
         url="https://raw.githubusercontent.com/dbt-labs/jaffle-shop-classic/refs/heads/main/seeds/raw_customers.csv",
-        duckdb=duckdb,
+        duckdb_path="/tmp/jaffle_platform.duckdb",
         table_name="jaffle_platform.main.raw_customers",
     )
 
 
-@dg.asset(
-    kinds={"duckdb"},
-    key=["target", "main", "raw_orders"],
-)
+@dg.asset(kinds={"duckdb"}, key=["target", "main", "raw_orders"])
 def raw_orders(duckdb: DuckDBResource) -> None:
     import_url_to_duckdb(
         url="https://raw.githubusercontent.com/dbt-labs/jaffle-shop-classic/refs/heads/main/seeds/raw_orders.csv",
-        duckdb=duckdb,
+        duckdb_path="/tmp/jaffle_platform.duckdb",
         table_name="jaffle_platform.main.raw_orders",
     )
 
 
-@dg.asset(
-    kinds={"duckdb"},
-    key=["target", "main", "raw_payments"],
-)
+@dg.asset(kinds={"duckdb"}, key=["target", "main", "raw_payments"])
 def raw_payments(duckdb: DuckDBResource) -> None:
     import_url_to_duckdb(
         url="https://raw.githubusercontent.com/dbt-labs/jaffle-shop-classic/refs/heads/main/seeds/raw_payments.csv",
-        duckdb=duckdb,
+        duckdb_path="/tmp/jaffle_platform.duckdb",
         table_name="jaffle_platform.main.raw_payments",
     )
+
